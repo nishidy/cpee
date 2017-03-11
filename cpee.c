acd2a70c3e7490f8ad594d436e66d42c9048d88a
nishidy <nishidyatutokyo@gmail.com>
2017-03-11 16:24:57 +0900

    Created archive.c.

--- cpee.c
+++ cpee.c
@@ -29,48 +29,6 @@ void commit_message(char* to_path, int argc, char** argv){
 	close(fd);
 }
 
-void archive_create(){
-	struct stat st;
-
-	g_archive = archive_write_new();
-	archive_write_add_filter_gzip(g_archive);
-	archive_write_set_format_pax_restricted(g_archive); // Note 1
-}
-
-void archive_register_filename(char* outname){
-	archive_write_open_filename(g_archive, outname);
-}
-
-void archive_add(char* from, char* to){
-	struct archive_entry *entry;
-	char buff[8192];
-	int len;
-	int fd;
-	struct stat s;
-
-	if(stat(from, &s)==-1)
-		show_errno();
-
-	entry = archive_entry_new(); // Note 2
-	archive_entry_set_pathname(entry, to);
-	archive_entry_copy_stat(entry,&s);
-	archive_write_header(g_archive, entry);
-	fd = open(from, O_RDONLY);
-	len = read(fd, buff, sizeof(buff));
-	while ( len > 0 ) {
-	    archive_write_data(g_archive, buff, len);
-	    len = read(fd, buff, sizeof(buff));
-	}
-	close(fd);
-	archive_entry_free(entry);
-}
-
-void archive_close(){
-	archive_write_close(g_archive); // Note 4
-	archive_write_free(g_archive); // Note 5
-}
-
-
 int is_file_exist(char* path){
 	struct stat s;
 	if( stat(path,&s) == -1 ){
